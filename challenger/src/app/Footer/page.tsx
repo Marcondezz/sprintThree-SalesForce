4e83ce55bd3641375edc9b6d1381dbf23b826f6f
Nicholas Lima <95785812+Nicholas-Liman@users.noreply.github.com>
2024-03-31 20:35:00 -0300

    Update page.tsx

--- challenger/src/app/Footer/page.tsx
+++ challenger/src/app/Footer/page.tsx
@@ -22,7 +22,6 @@ const Footer = () => {
                 <Link href="/TrailFinder"><p className="trailblazers-footer">Trailfinder</p></Link>
                 <Link href="/Pagina_Inicial"><p className="contato-footer">Inicial</p></Link>
             </div>
-            <div>
                 <div className="logos-footer">
                     <Link href="/Desenvolvedores"><p className="sustentabilidade-footer">Desenvo</p></Link>
                         <div className="todas-redesSociais-footer">
